2e934777e39498e46f7c6e6e1a41fbc158725f4c
gilibarak280 - Gili Barak <gilibarak280@gmail.com>
2016-07-17 18:02:40 +0300

    list with firebase ui

--- app/src/main/java/com/example/gili/fishingnet/ListViewFragment.java
+++ app/src/main/java/com/example/gili/fishingnet/ListViewFragment.java
@@ -7,17 +7,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.databind.type.ArrayType;
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.firebase.ui.FirebaseListAdapter;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 /**
  * Created by Gili on 17/07/2016.
@@ -39,23 +44,53 @@ public class ListViewFragment extends Fragment {
         LinearLayout listLayout = (LinearLayout) inflater.inflate(R.layout.fragment_list, container, false);
 
         mRootRef = new Firebase("https://fishingnet-dd809.firebaseio.com/listview");
-        mTextView = (TextView)listLayout.findViewById(R.id.textView4);
+
         mListView = (ListView)listLayout.findViewById(R.id.listView);
+        mTextView = (TextView)listLayout.findViewById(R.id.textView4);
+        //final ArrayAdapter<String> adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, mMessages);
+        //mListView.setAdapter(adapter);
 
         Firebase messagesRef = mRootRef.child("messages");
-        messagesRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String message = dataSnapshot.getValue(String.class);
-                Log.v("E_VALUE",message);
-                mTextView.setText(message);
-            }
 
+        FirebaseListAdapter<String> adapter = new FirebaseListAdapter<String>(getActivity(),String.class,android.R.layout.simple_list_item_1,messagesRef) {
             @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
+            protected void populateView(View view, String s, int i) {
+                TextView text = (TextView)view.findViewById(android.R.id.text1);
+                text.setText(s);
             }
-        });
+        };
+        mListView.setAdapter(adapter);
+//        messagesRef.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                String message = dataSnapshot.getValue(String.class);
+//                Log.v("E_CHILD_ADDED", message);
+//                mMessages.add(message);
+//                adapter.notifyDataSetChanged();
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//                String message = dataSnapshot.getValue(String.class);
+//                Log.v("E_CHILD_CHANGED", message);
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//                String message = dataSnapshot.getValue(String.class);
+//                Log.v("E_CHILD_REMOVED", message);
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(FirebaseError firebaseError) {
+//
+//            }
+//        });
 
         return listLayout;
     }
