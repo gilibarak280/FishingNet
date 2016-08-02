f6b574c900610312c9b1949803a7667d8fd9e9e0
gilibarak280 - Gili Barak <gilibarak280@gmail.com>
2016-08-02 17:33:14 +0300

    reports - with picture

--- app/src/main/java/com/example/gili/fishingnet/ReportModel.java
+++ app/src/main/java/com/example/gili/fishingnet/ReportModel.java
@@ -1,32 +1,25 @@
 package com.example.gili.fishingnet;
 
-import android.media.Image;
-
 /**
  * Created by Gili on 27/07/2016.
  */
 public class ReportModel {
 
-    //Image image;
     // Pass a custom Java object to Firebase: the class that defines it need a default constructor
     // that takes no arguments and has public getters for the properties to be assigned.
+
+    public String imageBitmapString;
     public String headline;
     public String description;
 
-//    public ReportModel(Image image,String headline,String description){
-//
-//        this.image = image;
-//        this.headline = headline;
-//        this.description = description;
-//    }
+    public ReportModel(String image,String headline,String description){
+
+        this.imageBitmapString = image;
+        this.headline = headline;
+        this.description = description;
+    }
 
     public ReportModel(){
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
-    }
-
-    public ReportModel(String headline,String description){
-
-        this.headline = headline;
-        this.description = description;
     }
 }
